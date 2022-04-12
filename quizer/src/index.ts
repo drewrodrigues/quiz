d8565a538fe9cfa0c6130441f6611f66dcf28777
Drew Rodrigues <thesimpledev@gmail.com>
2022-04-12 14:11:15 +0200

    Naive routing

--- quizer/src/index.ts
+++ quizer/src/index.ts
@@ -1,6 +1,14 @@
+import { QuizBuilder } from "./quizBuilder";
 import { Quizer } from "./quizer";
-import { render } from "./simpleReact";
+import { SimpleReact } from "./simpleReact";
 
+// ? thoughts: think about keeping track of the parent and index so we can easily replace elements in the dom (re-render)
+
+// ! naive routing -- improve this
 document.addEventListener("DOMContentLoaded", () => {
-  render("#index", Quizer());
+  if (window.location.search) {
+    SimpleReact.render("#index", Quizer);
+  } else {
+    SimpleReact.render("#index", QuizBuilder);
+  }
 });
