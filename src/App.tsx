24969ce991c3e7d02907378a9c3928614ce5d221
Drew Rodrigues <thesimpledev@gmail.com>
2022-04-14 14:09:28 +0200

    Add some styles & routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,47 @@
+import clsx from "clsx";
 import React from "react";
+import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
 import { QuizBuilder } from "./pages/builderPage";
 
 function App() {
   return (
-    <div className="m-[20px] rounded bg-gray-50 shadow">
-      <main className="">
-        <QuizBuilder />
-      </main>
-    </div>
+    <BrowserRouter>
+      <header className="bg-blue-200 p-[20px] mb-[20px]">
+        <div className="flex justify-between items-center max-w-screen-sm m-auto">
+          <main>
+            <a href="/" className="font-bold">
+              Quizer
+            </a>
+          </main>
+
+          <aside className="text-[14px]">
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                clsx(isActive && "border-b-[2px] border-blue-300")
+              }
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to="/builder"
+              className={({ isActive }) =>
+                clsx(isActive && "border-b-[2px] border-blue-300", "ml-[10px]")
+              }
+            >
+              Builder
+            </NavLink>
+          </aside>
+        </div>
+      </header>
+
+      <div className="max-w-screen-sm m-auto">
+        <Routes>
+          <Route path="/builder" element={<QuizBuilder />} />
+          <Route path="/" element={<p>Landing page</p>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/pages/builderPage.tsx
+++ src/pages/builderPage.tsx
@@ -43,75 +43,70 @@ export function QuizBuilder() {
     e.preventDefault();
     const newQuestions = [...quiz.questions];
     newQuestions.splice(index, 1);
-    setQuiz((q) => ({ ...quiz, questions: newQuestions }));
+    setQuiz(() => ({ ...quiz, questions: newQuestions }));
   }
 
   return (
-    <form className="">
-      <header className="flex justify-between items-center px-[40px] pt-[40px]">
+    <>
+      <header className="flex justify-between items-center mb-[20px]">
         <h1 className="font-bold text-[32px]">Builder</h1>
         <button className="border cursor-pointer p-[5px] rounded-half">
           View
         </button>
       </header>
 
-      <hr className="my-[40px] border" />
+      <form className="rounded bg-gray-50 shadow">
+        <section className="px-[40px] pt-[40px]">
+          <LabelAndInput
+            label="Title"
+            value={quiz.title}
+            onChange={(val) => setQuiz((q) => ({ ...q, title: val }))}
+          />
+          <LabelAndInput
+            label="Subtitle"
+            value={quiz.subtitle}
+            onChange={(val) => setQuiz((q) => ({ ...q, subtitle: val }))}
+          />
+        </section>
 
-      <section className="px-[40px]">
-        <LabelAndInput
-          label="Title"
-          value={quiz.title}
-          onChange={(val) => setQuiz((q) => ({ ...q, title: val }))}
-        />
-        <LabelAndInput
-          label="Subtitle"
-          value={quiz.subtitle}
-          onChange={(val) => setQuiz((q) => ({ ...q, subtitle: val }))}
-        />
-      </section>
-
-      {quiz.questions.map((question, questionIndex) => (
-        <>
-          <hr className="my-[40px] border" />
-
-          <section className="px-[40px]">
-            <button
-              className="rounded-half p-[5px] bg-red-200 text-red-500 text-[12px] mb-[20px]"
-              onClick={(e) => onDeleteQuestionClick(e, questionIndex)}
-            >
-              Delete Question
-            </button>
-
-            <LabelAndInput
-              label="Question"
-              value={quiz.subtitle}
-              onChange={(val) => setQuiz((q) => ({ ...q, subtitle: val }))}
-            />
-
-            <AnswerSet
-              answers={question.answerOptions}
-              onUpdate={() => null}
-              onDeleteAnswer={(answerIndex) =>
-                onDeleteAnswer(questionIndex, answerIndex)
-              }
-            />
-
-            <button
-              className="rounded-half p-[5px] bg-green-200 text-green-500 text-[12px]"
-              onClick={(e) => onNewAnswer(e, questionIndex)}
-            >
-              Add Answer
-            </button>
-          </section>
-        </>
-      ))}
-
-      <button
-        className="p-[10px] border-t mt-[40px] flex justify-center bg-gray-200 text-gray-500 text-[12px] rounded-bl rounded-br w-full"
-        onClick={onNewQuestionClick}
-      >
-        Add Question
-      </button>
-    </form>
+        {quiz.questions.map((question, questionIndex) => (
+          <>
+            <hr className="my-[40px] border" />
+            <section className="px-[40px]">
+              <button
+                className="rounded-half p-[5px] bg-red-200 text-red-500 text-[12px] mb-[20px]"
+                onClick={(e) => onDeleteQuestionClick(e, questionIndex)}
+              >
+                Delete Question
+              </button>
+              <LabelAndInput
+                label="Question"
+                value={quiz.subtitle}
+                onChange={(val) => setQuiz((q) => ({ ...q, subtitle: val }))}
+              />
+              <AnswerSet
+                answers={question.answerOptions}
+                onUpdate={() => null}
+                onDeleteAnswer={(answerIndex) =>
+                  onDeleteAnswer(questionIndex, answerIndex)
+                }
+              />
+              <button
+                className="rounded-half p-[5px] bg-green-200 text-green-500 text-[12px]"
+                onClick={(e) => onNewAnswer(e, questionIndex)}
+              >
+                Add Answer
+              </button>
+            </section>
+          </>
+        ))}
+        <button
+          className="p-[10px] border-t mt-[40px] flex justify-center bg-gray-200 text-gray-500 text-[12px] rounded-bl rounded-br w-full"
+          onClick={onNewQuestionClick}
+        >
+          Add Question
+        </button>
+      </form>
+    </>
   );
 }
